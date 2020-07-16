4eb32052da43288bf9088b68de16e991b43600dc
Ali Ariff <ali.ariff12@gmail.com>
2020-07-16 20:00:14 +0200

    update

--- bin/_docker.sh
+++ bin/_docker.sh
@@ -31,6 +31,11 @@ docker_repo() {
         name="$DOCKER_REGISTRY/$name"
     fi
 
+    if [ $repo == "go-deps" ] || [ $repo == "base" ]; then
+        echo "gcr.io/linkerd-io/$repo"
+        exit 0
+    fi
+
     echo "$name"
 }
 
